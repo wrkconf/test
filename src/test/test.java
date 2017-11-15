7ef5549cf703fcf1f7c2cb9068d25b2addef1dce
wrkconf <wrk.conf@gmail.com>
2017-11-15 11:58:57 +0400

    test2

--- src/test/test.java
+++ src/test/test.java
@@ -1,10 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class test {
-
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
-        
+        JFrame frame = new JFrame("Тестовый фрейм");
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-
-
 }
